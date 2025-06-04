c0c95c89c8890ceffcab38d411d25a196b779056
Hayato Doi <doi.hayato.0102@gmail.com>
2025-06-04 20:09:46 +0900

    修正2

--- test.java
+++ test.java
@@ -1,10 +1,6 @@
 
 public class test{
 	public static void main(String[] args){
-<<<<<<< HEAD
-		System.out.println("Hello from the main branch");
-=======
 		System.out.println("Hello from the local branch!");
->>>>>>> 5da4f4c (私たちの修正)
 	}
 }
